e0f27fda0c7aeade6aa7e5488ddc87bc3356b3ab
Ethan <61071128+Ethanoj1@users.noreply.github.com>
2021-03-13 17:26:28 -0800

    Add Discord Fix

--- Enable.lua
+++ Enable.lua
@@ -1,1 +1,9 @@
 print("Checking")
+if not _G.MadeAnnouncement then
+  game.StarterGui:SetCore("SendNotification", {
+      Title = "Discord Fix";
+      Text = "Sorry about that! The Request Discord Invite should work now.";
+      Duration = 10;
+  }) 
+  _G.MadeAnnouncement = true
+end
