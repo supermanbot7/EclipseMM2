3d180b60649078f44c786d0d2a86b4043fe47c73
Ethan <61071128+Ethanoj1@users.noreply.github.com>
2021-04-24 23:03:02 -0700

    Update Enable.lua

--- Enable.lua
+++ Enable.lua
@@ -1,9 +1,10 @@
 print("Checking")
-if not _G.MadeAnnouncement then
+--[[if not _G.MadeAnnouncement then
   game.StarterGui:SetCore("SendNotification", {
       Title = "Big Update Soon";
       Text = "Revamping autofarm and fixing many bugs soon.";
       Duration = 5;
   }) 
   _G.MadeAnnouncement = true
-end
+end]]
+--Too many idiots asking for when the update will come out
